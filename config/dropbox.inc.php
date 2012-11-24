3c985e51d38ffd3aff10ac9f081d1668c7061c16
nbussman <nbussman@uos.de>
2012-11-24 10:38:01 +0100

    winzig

---
+++ config/dropbox.inc.php
@@ -0,0 +1,5 @@
+<?php
+	// Dropbox settings 
+	$consumerKey     = '5wty9mf06gcuco0';
+	$consumerSecret  = 'hveok3hllw48hji';
+?>
---
+++ views/mails/write.php
@@ -0,0 +1,70 @@
+<?
+
+$this->set_layout("layouts/mail_list");
+$page_title = "Neue Nachricht";
+$page_id = "mail-write";
+
+?>
+  
+<div data-role="page" id="<?= $page_id ?: '' ?>">
+	<div data-role="header" data-theme="a">
+        	<a href="<?= $controller->url_for("quickdial") ?>" rel="external" data-icon="grid" data-iconpos="notext" data-theme="c"><?=_("Menu")?></a>
+        	<h1><?=$page_title ?: 'Stud.IP' ?></h1>
+	</div><!-- /header -->
+      <div data-role="content">
+      <?
+      		if (!empty( $empfData ))
+      		{
+      			// adressat wurde gewählt
+	      		?>
+	      			<p>
+	      				<div class="ui-grid-b a_bit_smaller_text" data-theme="c" style="font-size:10pt;">
+                        	<div class="ui-block-a">
+	                        	<img  src="
+	                        		<?= $controller->url_for("avatars/show", $empfData["user_id"], 'medium') ?>" 
+	                        		alt="Profil-Bild">
+                        	</div>
+                        	<div class="ui-block-b">
+	                        	<h3>Empfänger: </h3><?=$empfData['vorname'] ?> <?=$empfData['nachname'] ?>
+                        	</div>
+                        </div><!-- /grid-a -->
+	      				<hr>
+	      				<h3>Betreff</h3>
+	      				<input name="title">
+	      				<h3>Nachricht</h3>
+	      				<textarea style="min-height:200px;"></textarea>
+	      				<button value="submit">Senden</button>
+	      				</p>
+	      		<?
+      		}
+      		else
+      		{
+	      		//kein adressat wurde gewählt, es werden alle teilnehmer aller besuchten veranstaltungen 
+	      		//zur auswahl gestellt
+	      		
+	      		?>
+				<ul id="courses" data-role="listview" data-filter="true" data-filter-placeholder="Suchen" data-divider-theme="d" >
+				<?
+					foreach ($members AS $member)   
+					{
+					?>
+				        <li>
+					        <a href=" <?= $controller->url_for("mail/write", htmlReady($member['user_id'])) ?>">
+						        <img src="<?= $controller->url_for("avatars/show", $member['user_id'], 'medium') ?>" class="ui-li-thumb">
+						        <h3><?=htmlReady($member["title_front"]) ?> <?=htmlReady($member['Vorname']) ?>  <?=htmlReady($member['Nachname'])?> </h3>
+						    </a>
+				        </li>
+				    <?
+				    }
+				    ?>
+				</ul>
+				<?
+      		}
+      
+      ?>
+	  </div>
+<?
+    //print footer
+    echo $this->render_partial('mails/footer.php');
+?>
+</div>
