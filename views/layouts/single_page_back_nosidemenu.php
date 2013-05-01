cc572a2ad49e51d8fa8c1038bc5521ffa13aedac
nbussman <nbussman@uos.de>
2013-05-01 10:42:46 +0200

    forget to add some files

---
+++ views/layouts/single_page_back_nosidemenu.php
@@ -0,0 +1,23 @@
+<?
+	require("layout_settings.php");
+?>
+<!DOCTYPE html>
+<html>
+ <?
+  require("head_normal.php");
+ ?>
+  <body>
+    
+    <div data-role="page" id="<?= $page_id ?: '' ?>" >
+      <div data-role="header"  data-theme="<?=TOOLBAR_THEME ?>" data-add-back-btn="true">
+        <a href="#" data-rel="back" data-icon="arrow-l" data-iconpos="notext" data-theme="d">Back</a>
+        <h1><?= $page_title ?: 'Stud.IP' ?></h1>
+       <a href="<?= $controller->url_for("quickdial") ?>" class="externallink" data-ajax="false" 
+          data-icon="grid" data-iconpos="notext" data-theme="d"><?=_("Menu")?></a>
+      </div><!-- /header -->
+      
+      <div data-role="content">
+        <?= $content_for_layout ?>
+      </div><!-- /content -->
+  </body>
+</html>
