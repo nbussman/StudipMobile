8ee5337f897c22e83eb44f665f9d1f0b0982adf6
nbussman <nbussman@uos.de>
2013-05-14 10:02:39 +0200

    license added

--- views/layouts/toolbar_edit.php
+++ views/layouts/toolbar_edit.php
@@ -1,3 +1,19 @@
+<?
+// Copyright (C) 2013  Nils Bussmann
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+?>
 <div data-role="header" data-theme="<?=TOOLBAR_THEME ?>">
         <a href="<?= $controller->url_for("quickdial") ?>" class="externallink" data-ajax="false" data-icon="grid" data-iconpos="notext" data-theme="<?=TOOLBAR_BUTTONS ?>"><?=_("Menu")?></a>
         <h1><?= $page_title ?: 'Stud.IP' ?></h1>
